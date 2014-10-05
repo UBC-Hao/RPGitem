12e5ba06124b0155f4376fe19be2fac08155c19c
Taka-Sab <1277832129@qq.com>
2014-10-05 18:36:29 +0800

    增加CallInventory

---
+++ src/com/qq/q1277832129/RPGItems/API/AutoSetUp.java
@@ -0,0 +1,18 @@
+package com.qq.q1277832129.RPGItems.API;/*
+ *@author:yudi
+ *@QQ:1277832129
+ *@Twitter:yudi_327
+ */
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class AutoSetUp {
+/*Link start!*/
+    public static void setupListener(){}
+    public static void callInventory(Player p){
+        Inventory inv = Bukkit.createInventory(null,9,"开始强化装备吧!");
+        
+    }
+}
