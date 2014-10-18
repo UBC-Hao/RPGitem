40516ec67759a23f0a0bc9f3f02e365be55357a4
Taka-Sab <1277832129@qq.com>
2014-10-18 12:15:30 +0800

    随机化物品准备中!

--- src/com/qq/q1277832129/RPGItems/Main.java
+++ src/com/qq/q1277832129/RPGItems/Main.java
@@ -4,6 +4,30 @@ package com.qq.q1277832129.RPGItems;/*
  *@Twitter:yudi_327
  */
 
-public class Main {
-/*Link start!*/
+import com.qq.q1277832129.RPGItems.API.Sender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Main extends JavaPlugin {
+public static FileConfiguration config;
+
+
+public static HashMap<String,Sender> map;
+    @Override
+    public void onEnable(){
+       config=getConfig();
+       List<String> list = config.getStringList("stats");
+       //stats:
+       //  - "stat:min-max"
+       for(String pointer:list){
+          String now = pointer.split(":")[0];
+          String maxmin = pointer.split(":")[1];
+          int min = Integer.parseInt(maxmin.split("-")[0]);
+          int max = Integer.parseInt(maxmin.split("-")[1]);
+          map.put(now,new Sender(min,max));
+       }
+    }
 }
