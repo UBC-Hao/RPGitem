66a1784959afcfa18cfcf000e3f3f9da45e3aefb
Taka-Sab <1277832129@qq.com>
2014-10-19 11:26:30 +0800

    修正不科学的错误

--- src/com/qq/q1277832129/RPGItems/API/Sender.java
+++ src/com/qq/q1277832129/RPGItems/API/Sender.java
@@ -4,21 +4,27 @@ package com.qq.q1277832129.RPGItems.API;/*
  *@Twitter:yudi_327
  */
 
+import java.util.List;
 import java.util.Random;
 
 public class Sender {
 /*Link start!*/
     int max;
     int min;
-    public Sender(int max,int min){
+    List<Integer> list;
+    public Sender(int max,int min,List list){
         this.max=max;
         this.min=min;
+        this.list=list;
     }
     public int getMax(){
         return max;
     }
     public int getMin(){
         return min;
+    }
+    public boolean allow(int id){
+        return list.contains(id);
     }
     public int random(){
         Random rand = new Random();
--- src/com/qq/q1277832129/RPGItems/Main.java
+++ src/com/qq/q1277832129/RPGItems/Main.java
@@ -8,26 +8,33 @@ import com.qq.q1277832129.RPGItems.API.Sender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class Main extends JavaPlugin {
+
 public static FileConfiguration config;
-
-
 public static HashMap<String,Sender> map;
+
     @Override
     public void onEnable(){
        config=getConfig();
        List<String> list = config.getStringList("stats");
        //stats:
-       //  - "stat:min-max"
+       //  - "stat:min-max-id-id````````"
        for(String pointer:list){
           String now = pointer.split(":")[0];
           String maxmin = pointer.split(":")[1];
           int min = Integer.parseInt(maxmin.split("-")[0]);
           int max = Integer.parseInt(maxmin.split("-")[1]);
-          map.put(now,new Sender(min,max));
+          int id[] = new int[maxmin.split("-").length];
+          for(int i=2 ;i<maxmin.split("-").length ;i++){
+              id[i-2]=Integer.parseInt(maxmin.split("-")[i]);
+          }
+          List list1 = Arrays.asList(id);
+          map.put(now,new Sender(min,max,list1));
        }
     }
 }
